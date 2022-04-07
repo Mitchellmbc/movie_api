0d9e200de789059e7213acdf3b8d15c9d3769a5e
Mitchellmbc <mitchelldye3@gmail.com>
2022-04-07 16:35:33 +0100

    removed excess files and added a create server line to index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const express = require('express'),
 
 const app = express();
 
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
+
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
 
 let topMovies = [
