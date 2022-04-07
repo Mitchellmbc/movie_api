db3a12c06eb5e41b52eaf6fa45fef0842959de3c
Mitchellmbc <mitchelldye3@gmail.com>
2022-04-07 14:00:22 +0100

    Exercise 2.4 code complete

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express'),
+      morgan = require('morgan'),
+      fs = require('fs'),
+      path = require('path');
+
+const app = express();
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
+
+let topMovies = [
+  {
+    title: 'Jutice League Zack Snyder Cut',
+    director: 'Zack Snyder'
+  },
+  {
+    title: 'Licorice Pizza',
+    director: 'Paul Thomas Anderson'
+  },
+  {
+    title: 'Dune',
+    director: 'Denis Villeneuve'
+  },
+  {
+    title: 'Spider-Man: No Way Home',
+    director: 'Jon Watts'
+  },
+  {
+    title: 'Undine',
+    director: 'Christian Petzold'
+  },
+  {
+    title: 'King Richard',
+    director: 'Reinaldo Marcus Green'
+  },
+  {
+    title: 'Mass',
+    director: 'Franz Kranz'
+  },
+  {
+    title: 'Identifying Features',
+    director: 'Fernanda Valadez'
+  },
+  {
+    title: 'Nightmare Alley',
+    director: 'Guillermo del Toro'
+  },
+  {
+    title: 'Parallel Mothers',
+    director: 'Pedro Almodovar'
+  }
+];
+
+const bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use(morgan('combiend', {stream: accessLogStream}));
+app.use(express.static('public'));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie catalogue!');
+});
+
+// app.get('/documentation', (req, res) => {
+//   res.sendFile('public/documentation.html', { root: __dirname });
+// });
